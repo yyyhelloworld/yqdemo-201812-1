b63e205dda1aeaee5c8f2f6edc637a360ab32920
yq <1006078789@qq.com>
2018-12-01 21:37:09 +0800

    2018/12/1 to branch 12/1 add test in to class TestStaticT.javas

--- src/main/java/cn/qxfive/test/TestStaticT.java
+++ src/main/java/cn/qxfive/test/TestStaticT.java
@@ -1,9 +1,20 @@
 package cn.qxfive.test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class TestStaticT {
     public static <T> T get(T t){
         System.out.println();
-
         return t;
+    }
+    public static void test(List<? extends Number> objects){
+        List<? extends Object> list=new ArrayList<String>();
+    }
+
+    public static void main(String[] args) {
+        ArrayList<String> strings=new ArrayList<>();
+        test(new ArrayList<Float>());
     }
 }
